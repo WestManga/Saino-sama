c365a0d3c0e8074efb9f75f606bff2fe6afc17d2
mhazm <mhazmiee@gmail.com>
2021-03-25 05:34:06 +0700

    custom prefix command

---
+++ commands/moderation/prefix-reset.js
@@ -0,0 +1,21 @@
+const prefixSchema = require('../../models/prefix')
+const prefix = require('../../config.json').prefix
+const { confirmation } = require('@reconlx/discord.js')
+
+module.exports = {
+    name : 'prefix-reset',
+    run : async(client, message) => {
+        message.channel.send("Are you sure you want to reset the prefix?").then(async (msg) => {
+            const emoji = await confirmation(msg, message.author, ['✅', '❌'], 10000)
+            if(emoji === '✅') {
+                msg.delete()
+                await prefixSchema.findOneAndDelete({ Guild : message.guild.id })
+                message.channel.send(`The Prefix has been reset to ${prefix}`)
+            }
+            if(emoji === '❌') {
+                msg.delete()
+                message.channel.send('Reset Prefix has been cancelled.')
+            }
+        })
+    }
+}
