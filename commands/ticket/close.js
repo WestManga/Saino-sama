04714bea34c4932ac52d45daaf21fe30ca531acc
mhazm <mhazmiee@gmail.com>
2021-03-25 15:22:16 +0700

    fix attachment log

--- commands/ticket/close.js
+++ commands/ticket/close.js
@@ -1,4 +1,5 @@
 const { Message, Client, MessageAttachment} = require('discord.js')
+const db = require('../../models/ticket')
 const fs = require('fs')
 
 module.exports = {
@@ -13,13 +14,13 @@ module.exports = {
         message.channel.send('Deleting ticket in 5 seconds.....')
         setTimeout(() => {
             message.channel.delete().then(async ch=> {
-                client.ticketTranscript.findOne({ Channel : ch.id }, async(err, data) => {
+                db.findOne({ Channel : ch.id }, async(err, data) => {
                     if(err) throw err;
                     if(data) {
                         fs.writeFileSync(`../${ch.id}.txt`, data.Content.join("\n\n"))
                         transcriptChannel.send(`${message.guild.members.cache.get(ch.name).user.username}'s ticket have been closed.`)
                         await transcriptChannel.send(new MessageAttachment(fs.createReadStream(`../${ch.id}.txt`)));
-                        client.ticketTranscript.findOneAndDelete({ Channel : ch.id })
+                        db.findOneAndDelete({ Channel : ch.id })
                     }
                 })
             })
