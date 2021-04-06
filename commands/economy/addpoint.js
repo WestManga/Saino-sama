770852829e225b816c17954b8092bd78481e466a
mhazm <mhazmiee@gmail.com>
2021-04-06 17:56:15 +0700

    add + remove points

---
+++ commands/economy/addpoint.js
@@ -0,0 +1,40 @@
+const { MessageEmbed } = require('discord.js');
+const User = require('../../models/User');
+
+module.exports = {
+	name: 'addpoint',
+	aliases: ['addp'],
+	category: 'economy',
+	usage: '<mention> <amount of point>',
+	description: 'Give Someone Point',
+	run: async (bot, message, args) => {
+		let member = message.guild.member(message.mentions.users.first());
+        let author = message.guild.members.cache.get(message.author.id);
+		if (!member) return message.channel.send('Please Mention A User');
+        if (!author.hasPermission("BAN_MEMBERS")) return;        
+		if (!args[1]) return message.channel.send('Please Enter Valid Number');
+		if (args[1] < 1)
+			return message.channel.send('You Need To Transfer More Than 1');
+		if (isNaN(args[1])) return message.channel.send('Thats Not A Number -_-');
+
+		let target = await User.findOne({
+			guildID: message.guild.id,
+			userID: member.id,
+		});
+
+		if (!target) return bot.nodb(member.user);
+
+		if (author.userID == member.id)
+			return message.channel.send('How Is That Possible');
+		if (member.user.bot) return message.channel.send('Its A Bot -_-');
+
+		let e = new MessageEmbed()
+			.setColor(process.env.COLOR)
+			.setDescription(
+				`**${message.author.username}**  Gives ${member.user.username} ${args[1]} Points`,
+			);
+		target.point += Math.floor(parseInt(args[1]));
+		target.save();
+		message.channel.send({ embed: e });
+	},
+};
