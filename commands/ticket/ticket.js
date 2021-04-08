e36bec9d8a6f67749d56e49ab1fd532e9197b8ed
mhazm <mhazmiee@gmail.com>
2021-04-08 11:12:58 +0700

    new

--- commands/ticket/ticket.js
+++ commands/ticket/ticket.js
@@ -11,7 +11,7 @@ module.exports = {
         if(ch) return message.channel.send('You already have a ticket open.')
         message.guild.channels.create(`${message.author.id}`, {
             type : 'text',
-            parent : '824261268851916821',
+            parent : '829567821003489320',
             permissionOverwrites : [
                 {
                     id : message.guild.id,
@@ -24,7 +24,7 @@ module.exports = {
             ]
         }).then(async channel=> {
             message.reply(`click <#${channel.id}> to view your ticket`).then(m => m.delete({ timeout : 5000 }))
-            channel.send(`<@&823881816742035487> Someone need help!`)
+            channel.send(`<@&787144815145517066> Someone need help!`)
             channel.send(`${message.author}, welcome to your ticket!`)
         })
     }
