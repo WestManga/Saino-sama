62dbd37542fcc778968b7b5f41fa720e7e763ca6
mhazm <mhazmiee@gmail.com>
2021-04-08 11:57:29 +0700

    aw

--- commands/ticket/ticket.js
+++ commands/ticket/ticket.js
@@ -15,16 +15,15 @@ module.exports = {
             permissionOverwrites : [
                 {
                     id : message.guild.id,
-                    deny : ['VIEW_CHANNEL']
+                    deny : ('VIEW_CHANNEL')
                 },
                 {
                     id : message.author.id,
-                    allow : ['VIEW_CHANNEL', 'SEND_MESSAGES', 'ADD_REACTIONS', 'ATTACH_FILES']
+                    allow : ("VIEW_CHANNEL", "ATTACH_FILES", "SEND_MESSAGES", "READ_MESSAGE_HISTORY")
                 }
             ]
         }).then(async channel=> {
             message.reply(`click <#${channel.id}> to view your ticket`).then(m => m.delete({ timeout : 5000 }))
-            channel.send(`<@&787144815145517066> Someone need help!`)
             channel.send(`${message.author}, welcome to your ticket!`)
         })
     }
