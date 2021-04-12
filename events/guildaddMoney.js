61c33c8df11021cea9b6f4aec37ec067d34e5119
mhazm <mhazmiee@gmail.com>
2021-04-12 22:38:56 +0700

    add 20second to get money from chat

--- events/guildaddMoney.js
+++ events/guildaddMoney.js
@@ -3,45 +3,56 @@ const client = require('../index');
 const User = require('../models/User');
 const Guild = require('../models/Guild');
 
+const cooldown = new Set();
+
+function addToCooldown(ID) {
+    cooldown.add(ID);
+    setTimeout(() => {
+        cooldown.delete(ID);
+    }, 20000 /* 20 seconds */);
+}
+
 client.on('message', async(message) => {
-        if(message.channel.parentID !== '781212267236163596') return;
-        let user = await User.findOne({
-            guildID: message.guild.id,
-            userID: message.author.id,
-        });
-    
-        let guild = await Guild.findOne({ 
-            guildID: message.guild.id,
-        });
-    
-        const chatmoneych = client.channels.cache.get(guild.chatmoneyChannel); 
+        if (message.channel.parentID !== '781212267236163596') return;
+        if(!cooldown.has(message.author.id)) {
+            addToCooldown(message.author.id);
+            let user = await User.findOne({
+                guildID: message.guild.id,
+                userID: message.author.id,
+            });
         
-        const moneylog = client.channels.cache.get(guild.moneyincomelogChannel);
-        const levelup = client.channels.cache.get(guild.levelUpChannel);
-    
-        // DUIT + EXP
-        let rand = Math.floor(Math.random() * 5) + 5;
-        let randexp = Math.floor(Math.random() * 4) + 3;
-    
-        user.money += rand;
-        user.xp += randexp;
-        user.messages++;
+            let guild = await Guild.findOne({ 
+                guildID: message.guild.id,
+            });
         
-        if (user.xp >= process.env.UPXP) {
+                        
+            const moneylog = client.channels.cache.get(guild.moneyincomelogChannel);
+            const levelup = client.channels.cache.get(guild.levelUpChannel);
+        
+            // DUIT + EXP
+            let rand = Math.floor(Math.random() * 5) + 5;
+            let randexp = Math.floor(Math.random() * 4) + 3;
+        
+            user.money += rand;
+            user.xp += randexp;
+            user.messages++;
+            
+            if (user.xp >= process.env.UPXP) {
+                let e = new MessageEmbed()
+                    .setColor(process.env.COLOR)
+                    .setDescription(
+                        `[:tada:] Congrats ${message.author.username} You Level Up`,
+                    );
+                levelup.send(e);
+                user.xp -= process.env.UPXP;
+                user.level += 1;
+            }
+        
             let e = new MessageEmbed()
-                .setColor(process.env.COLOR)
-                .setDescription(
-                    `[:tada:] Congrats ${message.author.username} You Level Up`,
-                );
-            levelup.send(e);
-            user.xp -= process.env.UPXP;
-            user.level += 1;
+            .setColor(process.env.COLOR)
+            .setDescription(`${message.author.username} recieve chat money + Rp. ${rand}\nGet exp: ${randexp}`)
+            moneylog.send({embed : e});
+            user.afk = false;
+            user.save();
         }
-    
-        let e = new MessageEmbed()
-        .setColor(process.env.COLOR)
-        .setDescription(`${message.author.username} recieve chat money + Rp. ${rand}\nGet exp: ${randexp}`)
-        moneylog.send({embed : e});
-        user.afk = false;
-        user.save();
 });
