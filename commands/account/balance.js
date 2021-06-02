d00b04a7874b17ac02978e1615530d505298cf47
Muhammad Hazmi <mhazmiee@gmail.com>
2021-06-02 20:34:47 +0700

    add case if user not have balance

--- commands/account/balance.js
+++ commands/account/balance.js
@@ -1,6 +1,4 @@
 const { MessageEmbed } = require("discord.js");
-const moment = require("moment");
-const mongoose = require("mongoose");
 const thanksSchema = require("../../models/thanks");
 const { determineSupporterTitle } = require("../../handlers/profilehelper");
 const { COLOR } = process.env;
@@ -19,46 +17,52 @@ module.exports = {
 
     const member = message.guild.member(user);
     let avatar = user.displayAvatarURL({ dynamic: true });
-    const nickname = member.nickname;
 
     if (member.bot) return message.channel.send("Its A Bot -_-");
 
-    let data = await User.findOne({
+    const data = await User.findOne({
       guildID: message.guild.id,
       userID: member.id,
     });
 
-    let datat = await thanksSchema.findOne({
+    const datathanks = await Thanks.findOne({
       userId: member.id,
       guildId: message.guild.id,
     });
 
-    let guildData = await Guild.findOne({ guildID: message.guild.id });
     if (!data) return client.nodb(member.user);
 
     const patreonSupporter = determineSupporterTitle(data.account.patreon);
 
-    let inline = true;
-    let e = new MessageEmbed()
-      .setAuthor(user.tag, avatar)
-      .setTitle(`${patreonSupporter}`)
-      .setColor(COLOR)
-      .addField(
-        "<:diamondegg:836507553378730035> Point",
-        `${data.point || 0} Points`,
-        inline
-      )
-      .addField(
-        "<:wallet:836507553357496321> Money",
-        `Rp. ${data.money || 0}`,
-        inline
-      )
-      .addField(
-        "<a:bintang:819855110754271252> Thanks",
-        `${datat.received || 0} Thanks`,
-        inline
-      )
-      .setTimestamp();
-    message.channel.send({ embed: e });
+    const mybalance = data.money;
+    const mythanks = datathanks.received;
+
+    const inline = true;
+
+      const embedbalance = new MessageEmbed()
+        .setAuthor(user.tag, avatar)
+        .setTitle(`${patreonSupporter}`)
+        .setColor(COLOR)
+        .addField(
+          "<:diamondegg:836507553378730035> Point",
+          `${data.point || 0} Points`,
+          inline
+        )
+        .addField(
+          "<:wallet:836507553357496321> Money",
+          `Rp. ${data.money}`,
+          inline
+        )
+        .addField(
+          "<a:bintang:819855110754271252> Thanks",
+          `${datathanks.received || 0} Thanks`,
+          inline
+        )
+        .setTimestamp();
+
+    if (!mybalance) return message.channel.send("You dont have a balance!");
+    else if (mybalance) {
+      message.channel.send({ embed: embedbalance });
+    }
   },
 };
