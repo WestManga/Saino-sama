9f594bdb22c974a8600c1f14b2150bb01d022c79
Muhammad Hazmi <mhazmiee@gmail.com>
2021-06-25 00:37:09 +0700

    simple vote

---
+++ commands/extra/vote.js
@@ -0,0 +1,53 @@
+const Discord = require("discord.js");
+const { COLOR } = process.env;
+
+module.exports = {
+  name: "vote",
+  category: "extra",
+  description: "Make vote!",
+  usage: "",
+  aliases: [""],
+  cooldown: 1000,
+  run: async (client, message, args) => {
+    try {
+        let member = message.author;
+        let avatar = member.avatarURL({ size: 4096, dynamic: true });
+
+      let msg = args;
+
+      let vote = new Discord.MessageEmbed()
+        .setAuthor(member.username, avatar)
+        .setColor(COLOR)
+        .setDescription(`**Petition:**\n${msg}`)
+        .setFooter(`Please vote! Click reaction for voting..\nVote ended on 1 minutes`)
+      let vote_send = await message.channel.send(vote);
+
+      await vote_send.react("👍");
+      await vote_send.react("👎");
+
+      const yes = (reaction, user) => reaction.emoji.name == "👍" && user.id === message.author.id;
+      const no = (reaction, user) => reaction.emoji.name == "👎" && user.id === message.author.id;
+
+      vote_send
+        .awaitReactions(yes, { time: 60000 })
+        .then((collected) => {
+        vote.addField('👍', `${collected.size} Vote`, true)
+        vote.setFooter(`Vote Ended!`)
+        vote_send.edit(vote);
+        })
+        .catch(console.error);
+
+        vote_send
+        .awaitReactions(no, { time: 60000 })
+        .then((collected) => {
+        vote.addField('👎', `${collected.size} Vote`, true)
+        vote.setFooter(`Vote Ended!`)
+        vote_send.edit(vote);
+        })
+        .catch(console.error);
+    } catch (err) {
+      console.log(err);
+      return;
+    }
+  },
+};
