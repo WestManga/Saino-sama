a7ecf75ebdefd0fcdaaaf3fcbebc19b014228144
Muhammad Hazmi <mhazmiee@gmail.com>
2021-09-07 16:49:37 +0700

    new check setting

--- commands/settings/checksetting.js
+++ commands/settings/checksetting.js
@@ -1,6 +1,7 @@
 const { Client, Message, MessageEmbed } = require("discord.js");
 const Schema = require("../../models/Guild");
 const { COLOR } = process.env;
+const { statusPTC } = require("../../handlers/profilehelper");
 
 module.exports = {
   name:'checksetting',
@@ -27,6 +28,12 @@ module.exports = {
         const modlog = client.channels.cache.get(data.modlogChannel);
         const moneylog = client.channels.cache.get(data.moneylogChannel);
         const moneyincomelog = client.channels.cache.get(data.moneyincomelogChannel);
+        const budgetserver = data.budget;
+        const welcomestatus = statusPTC(data.active.welcome);
+        const byestatus = statusPTC(data.active.bye);
+        const applystatus = statusPTC(data.active.apply);
+        const convertstatus = statusPTC(data.active.convert);
+        const givemoneystatus = statusPTC(data.active.give);
 
         const prefix = data.prefix;
 
@@ -39,12 +46,25 @@ module.exports = {
         .addField("Fined Max", data.fined.max, true)
         .addField("Money Income Min", data.money.min, true)
         .addField("Money Income Max", data.money.max, true)
-        .addField("Welcome Channel", welcomeChannel)
-        .addField("Levelup Channel", levelup)
-        .addField("ModLog Channel", modlog)
-        .addField("MoneyLog Channel", moneylog)
-        .addField("Money Income Log", moneyincomelog)
+        .addField("Budget PTC", `Rp.${budgetserver}`, true)
+        .addField("Welcome Channel", welcomeChannel, true)
+        .addField("Levelup Channel", levelup, true)
+        .addField("ModLog Channel", modlog, true)
+        .addField("MoneyLog Channel", moneylog, true)
+        .addField("Money Income Log", moneyincomelog, true)
         message.channel.send({embed: e});
+
+        // ACTIVE or DEACTIVE
+        let info = new MessageEmbed()
+        .setTitle(`There is feature active or deactive for guild ${guildname}`)
+        .setDescription(`> You can active this feature with ${prefix}enable <function> \n> You can deactive this feature with ${prefix}disable <function>`)
+        .setColor(COLOR)
+        .addField("Welcome Message", welcomestatus, true)
+        .addField("Bye Message", byestatus, true)
+        .addField("Apply Staff", applystatus, true)
+        .addField("Convert Money", convertstatus, true)
+        .addField("Give Money", givemoneystatus, true)
+        message.channel.send({ embed: info });
     })
   }
 };
