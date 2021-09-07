33f033ab2ae321aa34e32243e11c9c1cdb17826b
Muhammad Hazmi <mhazmiee@gmail.com>
2021-09-07 18:45:41 +0700

    change and add wmpj and popular

--- commands/animanga/westmanga.js
+++ commands/animanga/westmanga.js
@@ -9,10 +9,27 @@ module.exports = {
   cooldown:10,
   run: async(client, message, args) => {
     try {
-        let query = args.join(' ');
-        if (!query) return message.reply('masukin keyword yang mau dicari dulu, baka!');
+        let type = args[0];
+        if (!type) {
+          let embed = new Discord.MessageEmbed()
+          .setColor(process.env.COLOR)
+          .setDescription('Gunakan tambahan s untuk mencari..')
+          let warning = await message.channel.send({embed: embed});
+          warning.delete({ timeout: 5000 });
+        }
+        
+        if (type.toLowerCase() === 'popular') {
+          await client.westmanga.getPopular(message);
+        } else
+        if (type.toLowerCase() === 'lastpjupdate', 'lastpj') {
+          await client.westmanga.getPJUpdate(message);
+        } else
+        if (type.toLowerCase() === 'search', 's') {
+          let query = args.slice(1).join(' ');
+          if (!query) return message.reply('masukin keyword yang mau dicari dulu, baka!');
 
-        await client.westmanga.getSearch(query, message);
+          await client.westmanga.getSearch(query, message);
+        }
     } catch (error) {
         return console.log(error);
         // Restart the bot as usual. 
