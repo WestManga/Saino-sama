65e14288b7bcf6256f255d86ccbdfd1c70bd6e14
Muhammad Hazmi <mhazmiee@gmail.com>
2021-09-07 18:46:12 +0700

    add getpopular and getnewupdate pj

--- handlers/Westmanga.js
+++ handlers/Westmanga.js
@@ -61,6 +61,114 @@ class Westmanga {
         })
     }
 
+    getPopular(message) {
+        return new Promise(async (fullfill, reject) => {
+            try {
+                const roleColor =
+                message.guild.me.displayHexColor === "#000000"
+                ? "#ffffff"
+                : message.guild.me.displayHexColor;
+                let get = await axios.get(`https://westapi.herokuapp.com/api/manga/popular`);
+                let data_search = get.data.manga_list;
+                if (data_search.length < 1) return message.reply(`Pencarian manga popular di Westmanga tidak ditemukan!`);
+
+                //get endpoint
+                let endpoint_search = [];
+                data_search.forEach(a => {
+                    endpoint_search.push(a.endpoint);
+                });
+                console.log(endpoint_search)
+
+                //send title results
+                let embed = new Discord.MessageEmbed()
+                    .setColor(roleColor)
+                    .setTitle('Hasil Pencarian')
+                    .setDescription(data_search.map((a, i) => `${i + 1}. ${a.title}`).join('\n'))
+                let embed_search = await message.channel.send(embed);
+                let alert_search = await message.reply('pilih untuk melanjutkan!');
+
+                let author = message.author;
+                let response = await message.channel.awaitMessages((m) => m.content.toLowerCase() && m.author.id === author.id, {
+                    max: 1,
+                    time: 100000,
+                    errors: ["time"]
+                }).catch((err) => {
+                    return message.reply('waktu permintaan telah habis!\nSilahkan buat Permintaan kembali!').then(t => {
+                        t.delete({ timeout: 5000 });
+                        embed_search.delete();
+                        alert_search.delete();
+                    });
+                });
+
+                const search_index = response.first().content;
+                let result_search = endpoint_search[search_index - 1];
+                await embed_search.delete();
+                await alert_search.delete();
+                await this.getDetail(result_search, message).then(t => {
+                    response.first().delete();
+                });
+
+                fullfill();
+            } catch (error) {
+                reject(error);
+            }
+        })
+    }
+
+    getPJUpdate(message) {
+        return new Promise(async (fullfill, reject) => {
+            try {
+                const roleColor =
+                message.guild.me.displayHexColor === "#000000"
+                ? "#ffffff"
+                : message.guild.me.displayHexColor;
+                let get = await axios.get(`https://westapi.herokuapp.com/api/manga/newupdate`);
+                let data_search = get.data.manga_list;
+                if (data_search.length < 1) return message.reply(`Pencarian manga popular di Westmanga tidak ditemukan!`);
+
+                //get endpoint
+                let endpoint_search = [];
+                data_search.forEach(a => {
+                    endpoint_search.push(a.endpoint);
+                });
+                console.log(endpoint_search)
+
+                //send title results
+                let embed = new Discord.MessageEmbed()
+                    .setColor(roleColor)
+                    .setTitle('Hasil Pencarian')
+                    .setDescription(data_search.map((a, i) => `${i + 1}. ${a.title}\`\`\`${a.chapter} | ${a.update_on}\`\`\``).join('\n'))
+                let embed_search = await message.channel.send(embed);
+                let alert_search = await message.reply('pilih untuk melanjutkan!');
+
+                let author = message.author;
+                let response = await message.channel.awaitMessages((m) => m.content.toLowerCase() && m.author.id === author.id, {
+                    max: 1,
+                    time: 100000,
+                    errors: ["time"]
+                }).catch((err) => {
+                    return message.reply('waktu permintaan telah habis!\nSilahkan buat Permintaan kembali!').then(t => {
+                        t.delete({ timeout: 5000 });
+                        embed_search.delete();
+                        alert_search.delete();
+                    });
+                });
+
+                const search_index = response.first().content;
+                let result_search = endpoint_search[search_index - 1];
+                await embed_search.delete();
+                await alert_search.delete();
+                await this.getDetail(result_search, message).then(t => {
+                    response.first().delete();
+                });
+
+                fullfill();
+            } catch (error) {
+                reject(error);
+            }
+        })
+    }
+
     getDetail(query, message) {
         return new Promise(async (fullfill, reject) => {
             try {
